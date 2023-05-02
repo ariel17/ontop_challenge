dab8569ccc84a20b9efb08864b3eb5bc54be02dd
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-02 04:36:54 -0300

    Adds more test cases.

--- src/test/java/ar/com/ariel17/core/services/transaction/TransactionServiceImplTest.java
+++ src/test/java/ar/com/ariel17/core/services/transaction/TransactionServiceImplTest.java
@@ -1,7 +1,9 @@
 package ar.com.ariel17.core.services.transaction;
 
 import ar.com.ariel17.core.clients.payment.PaymentProviderApi;
+import ar.com.ariel17.core.clients.payment.PaymentProviderApiException;
 import ar.com.ariel17.core.clients.wallet.WalletApi;
+import ar.com.ariel17.core.clients.wallet.WalletApiException;
 import ar.com.ariel17.core.domain.Payment;
 import ar.com.ariel17.core.domain.bank.BankAccount;
 import ar.com.ariel17.core.domain.bank.BankAccountOwner;
@@ -10,8 +12,10 @@ import ar.com.ariel17.core.domain.transaction.Transaction;
 import ar.com.ariel17.core.domain.transaction.Type;
 import ar.com.ariel17.core.repositories.LockRepository;
 import ar.com.ariel17.core.repositories.movement.MovementRepository;
+import ar.com.ariel17.core.repositories.movement.MovementRepositoryException;
 import ar.com.ariel17.core.repositories.payment.PaymentRepository;
 import ar.com.ariel17.core.repositories.recipient.RecipientRepository;
+import ar.com.ariel17.core.repositories.recipient.RecipientRepositoryException;
 import ar.com.ariel17.core.services.lock.LockService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,8 +27,7 @@ import java.math.BigDecimal;
 import java.util.Currency;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
@@ -79,12 +82,11 @@ public class TransactionServiceImplTest {
         account = new BankAccount(1234L, 1234L, Currency.getInstance("USD"));
         recipient = new BankAccountOwner(null, userId, account, "1234", "John", "Doe", null);
         amount = new BigDecimal(1000);
-
-        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
     }
 
     @Test
     public void testTransfer_ok() throws Exception {
+        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
         when(lockRepository.acquire()).thenReturn(true);
         when(walletAPI.getBalance(eq(userId))).thenReturn(new BigDecimal(5000));
 
@@ -102,7 +104,6 @@ public class TransactionServiceImplTest {
 
         verify(recipientRepository, times(1)).save(eq(recipient));
 
-        verify(lockRepository, times(1)).acquire();
         verify(lockRepository, times(1)).close();
 
         for (Movement m: transaction.getMovements()) {
@@ -113,5 +114,120 @@ public class TransactionServiceImplTest {
                 assertEquals(paymentId, m.getPaymentId());
             }
         }
+    }
+
+    @Test
+    public void testTransfer_recipientRepositoryFails() throws RecipientRepositoryException {
+        doThrow(new RecipientRepositoryException("mocked exception")).when(recipientRepository).save(eq(recipient));
+
+        assertThrows(TransactionException.class, () -> service.transfer(userId, recipient, amount));
+    }
+
+    @Test
+    public void testTransfer_lockNotAcquired() throws Exception {
+        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
+        when(lockRepository.acquire()).thenReturn(false);
+
+        assertThrows(TransactionException.class, () -> service.transfer(userId, recipient, amount));
+
+        verify(lockRepository, times(1)).close();
+    }
+
+    @Test
+    public void testTransfer_insufficientBalance() throws Exception {
+        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
+        when(lockRepository.acquire()).thenReturn(true);
+        when(walletAPI.getBalance(eq(userId))).thenReturn(new BigDecimal(200));
+
+        assertThrows(TransactionException.class, () -> service.transfer(userId, recipient, amount));
+
+        verify(lockRepository, times(1)).close();
+    }
+
+    @Test
+    public void testTransfer_getBalanceFails() throws Exception {
+        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
+        when(lockRepository.acquire()).thenReturn(true);
+        doThrow(new WalletApiException("mocked exception")).when(walletAPI).getBalance(eq(userId));
+
+        assertThrows(TransactionException.class, () -> service.transfer(userId, recipient, amount));
+
+        verify(lockRepository, times(1)).close();
+    }
+
+    @Test
+    public void testTransfer_walletTransactionFails() throws Exception {
+        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
+        when(lockRepository.acquire()).thenReturn(true);
+        when(walletAPI.getBalance(eq(userId))).thenReturn(new BigDecimal(5000));
+
+        BigDecimal total = BigDecimal.valueOf(-1100.0);
+        doThrow(new WalletApiException("mocked exception")).when(walletAPI).createTransaction(eq(userId), eq(total));
+
+        assertThrows(TransactionException.class, () -> service.transfer(userId, recipient, amount));
+
+        verify(lockRepository, times(1)).close();
+    }
+
+    @Test
+    public void testTransfer_paymentProviderFails() throws Exception {
+        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
+        when(lockRepository.acquire()).thenReturn(true);
+        when(walletAPI.getBalance(eq(userId))).thenReturn(new BigDecimal(5000));
+
+        BigDecimal total = BigDecimal.valueOf(-1100.0);
+        Long walletTransactionId = 555L;
+        when(walletAPI.createTransaction(eq(userId), eq(total))).thenReturn(walletTransactionId);
+
+        doThrow(new PaymentProviderApiException("mocked error")).when(paymentProviderAPI).createPayment(eq(sourceOwner), eq(recipient), eq(amount));
+        assertThrows(TransactionException.class, () -> service.transfer(userId, recipient, amount));
+
+        verify(walletAPI, times(1)).createTransaction(eq(userId), eq(total));
+        verify(walletAPI, times(1)).createTransaction(eq(userId), eq(total.negate()));
+
+        verify(lockRepository, times(1)).close();
+    }
+
+    @Test
+    public void testTransfer_paymentProviderFailsAndLaterWalletFails() throws Exception {
+        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
+        when(lockRepository.acquire()).thenReturn(true);
+        when(walletAPI.getBalance(eq(userId))).thenReturn(new BigDecimal(5000));
+
+        BigDecimal total = BigDecimal.valueOf(-1100.0);
+        Long walletTransactionId = 555L;
+        when(walletAPI.createTransaction(eq(userId), eq(total))).thenReturn(walletTransactionId).
+                thenThrow(new WalletApiException("mocked wallet exception"));
+
+        doThrow(new PaymentProviderApiException("mocked provider error")).when(paymentProviderAPI).createPayment(eq(sourceOwner), eq(recipient), eq(amount));
+        assertThrows(TransactionException.class, () -> service.transfer(userId, recipient, amount));
+
+        verify(walletAPI, times(1)).createTransaction(eq(userId), eq(total));
+        verify(walletAPI, times(1)).createTransaction(eq(userId), eq(total.negate()));
+
+        verify(lockRepository, times(1)).close();
+    }
+
+    @Test
+    public void testTransfer_movementRepositoryFails() throws Exception {
+        when(lockService.createLockForUserId(eq(userId))).thenReturn(lockRepository);
+        when(lockRepository.acquire()).thenReturn(true);
+        when(walletAPI.getBalance(eq(userId))).thenReturn(new BigDecimal(5000));
+
+        BigDecimal total = BigDecimal.valueOf(-1100.0);
+        Long walletTransactionId = 555L;
+        when(walletAPI.createTransaction(eq(userId), eq(total))).thenReturn(walletTransactionId);
+
+        UUID paymentId = UUID.randomUUID();
+        Payment response = new Payment(paymentId, new BigDecimal(3999), "ok", null, null);
+        when(paymentProviderAPI.createPayment(eq(sourceOwner), eq(recipient), eq(amount))).thenReturn(response);
+
+        doThrow(new MovementRepositoryException("mocked exception")).when(movementRepository).save(any(Transaction.class));
+
+        assertThrows(TransactionException.class, () -> service.transfer(userId, recipient, amount));
+
+        verify(recipientRepository, times(1)).save(eq(recipient));
+
+        verify(lockRepository, times(1)).close();
     }
 }
