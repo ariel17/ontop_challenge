29c23410a27c73aade48eef0e3b96ac627c52c30
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-02 17:41:13 -0300

    Adds required beans for lock repository.

--- src/main/java/ar/com/ariel17/ontop/AppConfig.java
+++ src/main/java/ar/com/ariel17/ontop/AppConfig.java
@@ -1,17 +1,32 @@
 package ar.com.ariel17.ontop;
 
+import ar.com.ariel17.ontop.adapters.repositories.LockRepositoryImpl;
 import ar.com.ariel17.ontop.core.domain.BankAccount;
 import ar.com.ariel17.ontop.core.domain.BankAccountOwner;
+import ar.com.ariel17.ontop.core.repositories.LockRepository;
 import ar.com.ariel17.ontop.core.services.TransactionFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.integration.redis.util.RedisLockRegistry;
+import org.springframework.integration.support.locks.ExpirableLockRegistry;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.util.Currency;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements ApplicationContextAware {
+
+    private static final String LOCK_NAME = "lock";
+
+    private ApplicationContext context;
 
     @Value("${on_top.routing}")
     private Long onTopRoutingNumber;
@@ -28,6 +43,10 @@ public class AppConfig {
     @Value("${transactions.fee_percent}")
     private String feePercent;
 
+
+    @Value("${lock.release_time_seconds}")
+    private int lockReleaseTimeSeconds;
+
     @Bean
     public BankAccountOwner onTopBankAccount() {
         BankAccount account = new BankAccount(onTopRoutingNumber, onTopAccountNumber, Currency.getInstance(currencyCode));
@@ -38,5 +57,24 @@ public class AppConfig {
     public TransactionFactory transactionFactory() {
         BigDecimal percent = new BigDecimal(feePercent);
         return new TransactionFactory(percent);
+    }
+
+    @Bean(destroyMethod = "destroy")
+    public ExpirableLockRegistry redisLockRegistry(RedisConnectionFactory redisConnectionFactory) {
+        var releaseTime = Duration.ofSeconds(lockReleaseTimeSeconds).toMillis();
+        return new RedisLockRegistry(redisConnectionFactory, LOCK_NAME, releaseTime);
+    }
+
+    @Bean
+    @DependsOn({"redisLockRegistry"})
+    @Scope("prototype")
+    public LockRepository lockRepository(Long userId) {
+        ExpirableLockRegistry lockRegistry = context.getBean(ExpirableLockRegistry.class);
+        return new LockRepositoryImpl(lockRegistry, userId);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context = context;
     }
 }
