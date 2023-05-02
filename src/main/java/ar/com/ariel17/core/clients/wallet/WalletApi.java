cfb39ed9eeab85716c124de25d919dc3130a53f3
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-02 03:18:51 -0300

    Adds external clients interfaces.

--- src/main/java/ar/com/ariel17/core/clients/wallet/WalletApi.java
+++ src/main/java/ar/com/ariel17/core/clients/wallet/WalletApi.java
@@ -1,7 +1,10 @@
 package ar.com.ariel17.core.clients.wallet;
 
+import org.springframework.stereotype.Component;
+
 import java.math.BigDecimal;
 
+@Component
 public interface WalletApi {
 
     BigDecimal getBalance(Long userId);
