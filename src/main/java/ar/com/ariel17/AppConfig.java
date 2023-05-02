8c61f01131a57766a422cef1fefcd100727a9909
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-02 04:07:07 -0300

    Adds custom bean configurations.

---
+++ src/main/java/ar/com/ariel17/AppConfig.java
@@ -0,0 +1,27 @@
+package ar.com.ariel17;
+
+import ar.com.ariel17.core.domain.bank.BankAccount;
+import ar.com.ariel17.core.domain.bank.BankAccountOwner;
+import ar.com.ariel17.core.services.transaction.TransactionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public BankAccountOwner onTopBankAccount() {
+        // TODO move parameters to config file
+        BankAccount account = new BankAccount(1234L, 1234L, Currency.getInstance("USD"));
+        return new BankAccountOwner(null, 0L, account, "", "ON TOP INC", "", null);
+    }
+
+    @Bean
+    public TransactionFactory transactionFactory() {
+        BigDecimal feePercent = new BigDecimal("0.1");
+        return new TransactionFactory(feePercent);
+    }
+}
