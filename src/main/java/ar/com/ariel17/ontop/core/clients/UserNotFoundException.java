dc8f4a59621836c662484ac82f16fe1bc175e9dd
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-04 03:25:17 -0300

    Adds not found exceptions.

---
+++ src/main/java/ar/com/ariel17/ontop/core/clients/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package ar.com.ariel17.ontop.core.clients;
+
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ar/com/ariel17/ontop/core/repositories/BankAccountOwnerNotFoundException.java
@@ -0,0 +1,8 @@
+package ar.com.ariel17.ontop.core.repositories;
+
+public class BankAccountOwnerNotFoundException extends Exception {
+
+    public BankAccountOwnerNotFoundException(String message) {
+        super(message);
+    }
+}
