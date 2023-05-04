1136e28e03925341fa9a584b70d657b15248246e
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-03 23:06:51 -0300

    Changes operation name.

--- src/test/java/ar/com/ariel17/ontop/core/domain/MovementTest.java
+++ src/test/java/ar/com/ariel17/ontop/core/domain/MovementTest.java
@@ -56,35 +56,35 @@ public class MovementTest extends ValidatorTest {
 
     @Test
     public void testIsValid_validFee() {
-        Movement m = new Movement(null, 1234L, Type.FEE, Operation.EGRESS, currency, amount, null, null, null, null, null);
+        Movement m = new Movement(null, 1234L, Type.FEE, Operation.WITHDRAW, currency, amount, null, null, null, null, null);
         Set<ConstraintViolation<Movement>> violations = validator.validate(m);
         assertEquals(0, violations.size());
     }
 
     @Test
     public void testIsValid_invalidFeeWithAccounts() {
-        Movement m = new Movement(null, 1234L, Type.FEE, Operation.EGRESS, currency, amount, account1, account2, null, null, null);
+        Movement m = new Movement(null, 1234L, Type.FEE, Operation.WITHDRAW, currency, amount, account1, account2, null, null, null);
         Set<ConstraintViolation<Movement>> violations = validator.validate(m);
         assertEquals(1, violations.size());
     }
 
     @Test
     public void testIsValid_validNotFee() {
-        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.EGRESS, currency, amount, account1, account2, null, null, null);
+        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.WITHDRAW, currency, amount, account1, account2, null, null, null);
         Set<ConstraintViolation<Movement>> violations = validator.validate(m);
         assertEquals(0, violations.size());
     }
 
     @Test
     public void testIsValid_sameAccounts() {
-        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.EGRESS, currency, amount, account1, account1, null, null, null);
+        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.WITHDRAW, currency, amount, account1, account1, null, null, null);
         Set<ConstraintViolation<Movement>> violations = validator.validate(m);
         assertEquals(1, violations.size());
     }
 
     @Test
     public void testIsComplete_complete() {
-        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.EGRESS, currency, amount, account1, account2, walletTransactionId, paymentId, null);
+        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.WITHDRAW, currency, amount, account1, account2, walletTransactionId, paymentId, null);
         Set<ConstraintViolation<Movement>> violations = validator.validate(m);
         assertEquals(0, violations.size());
         assertTrue(m.isComplete());
@@ -92,7 +92,7 @@ public class MovementTest extends ValidatorTest {
 
     @Test
     public void testIsComplete_incomplete() {
-        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.EGRESS, currency, amount, account1, account2, null, null, null);
+        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.WITHDRAW, currency, amount, account1, account2, null, null, null);
         Set<ConstraintViolation<Movement>> violations = validator.validate(m);
         assertEquals(0, violations.size());
         assertFalse(m.isComplete());
@@ -100,13 +100,13 @@ public class MovementTest extends ValidatorTest {
 
     @Test
     public void testSetPaymentId_nonFee() {
-        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.EGRESS, currency, amount, account1, account2, null, null, null);
+        Movement m = new Movement(null, 1234L, Type.TRANSFER, Operation.WITHDRAW, currency, amount, account1, account2, null, null, null);
         m.setPaymentId(paymentId);
     }
 
     @Test
     public void testSetPaymentId_fee() {
-        Movement m = new Movement(null, 1234L, Type.FEE, Operation.EGRESS, currency, amount, null, null, null, null, null);
+        Movement m = new Movement(null, 1234L, Type.FEE, Operation.WITHDRAW, currency, amount, null, null, null, null, null);
         assertThrows(IllegalArgumentException.class, () -> m.setPaymentId(paymentId));
     }
 }
