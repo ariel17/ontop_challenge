06ed7a315405f98331b3bbccc93267eabdfcafb2
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-04 21:55:28 -0300

    Adds TODO.

--- src/main/java/ar/com/ariel17/ontop/core/repositories/BankAccountRepository.java
+++ src/main/java/ar/com/ariel17/ontop/core/repositories/BankAccountRepository.java
@@ -5,4 +5,5 @@ import ar.com.ariel17.ontop.core.domain.BankAccountOwner;
 public interface BankAccountRepository extends DatabaseRepository<BankAccountOwner> {
 
     BankAccountOwner getById(Long id) throws BankAccountOwnerNotFoundException;
+    // TODO add contraint for only 1 routing+account+user
 }
